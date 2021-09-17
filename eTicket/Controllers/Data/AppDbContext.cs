146760ba77abb1eef9ee8b4101e82761b0fcd686
adegboyefoluso <foluso.o.adegboye@gmail.com>
2021-09-17 00:21:29 -0400

    second

---
+++ eTicket/Controllers/Data/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using eTicket.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Controllers.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //Configure primary key for the joining table
+            modelBuilder.Entity<ActorMovie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            //Configure relationship between the joining table 
+            modelBuilder.Entity<ActorMovie>()
+                 .HasOne(m => m.Movie)
+                 .WithMany(am => am.ActorMovies)
+                 .HasForeignKey(m => m.MovieId); // Foreign key  to movie is the Movie Id
+
+            modelBuilder.Entity<ActorMovie>()
+                .HasOne(m => m.Actor)
+                .WithMany(am => am.ActorMovies)
+                .HasForeignKey(m => m.ActorId);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<ActorMovie> ActorMovies { get; set; }
+
+    }
+}
---
+++ eTicket/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using eTicket.Controllers.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTicket.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+        //RelationShip
+        public List<ActorMovie> ActorMovies { get; set; }
+
+        //Cinema Relationship
+        public int CinemaId { get; set; }
+        public Cinema Cinema { get; set; }
+
+        //Producer Relationship
+        public int ProducerId { get; set; }
+        public Producer  Producer { get; set; }
+
+    }
+}
